3113e1971eb503fb6a139e656ab5dd6edcec874a
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-02-14 20:37:46 +0330

    feat: add confetti

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+import confetti from "@hiseb/confetti";
+
 const addressInput = document.getElementById("addressInput");
 const copyBtn = document.getElementById("copyBtn");
 
-copyBtn.addEventListener("click", function () {
+copyBtn.addEventListener("click", function(e) {
   navigator.clipboard.writeText(addressInput.value);
   copyBtn.textContent = "✔ Copied";
-  
+  confetti({
+    position: { x: e.clientX, y: e.clientY },
+    count: 100,
+    size: 1.2,
+    velocity: 150,
+    fade: false,
+  });
+
   setTimeout(() => {
     copyBtn.textContent = "Copy";
     copyBtn.blur();
